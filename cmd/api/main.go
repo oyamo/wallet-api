1f54137f269effa842485216daa0fdd3d1192526
oyamoh-brian <oyamobrian8@gmail.com>
2022-03-26 17:29:51 +0300

    add commandline file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+// @title Wallet API
+// @version 1.0
+// @description An API for managing wallets of players
+// @contact.name Oyamo Brian
+// @contact.url https://github.com/oyamo
+// @contact.email oyamo.xyz@gmail.com
+// @BasePath /api/v1
+func main() {
+	log.Infoln("Starting server")
+}
